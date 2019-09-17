6b82d3e76f7af009af1ad9cc816584cf61b594ab
Dylan McNeil <dylan_mcneil@hotmail.co.uk>
2019-09-17 22:54:11 +0100

    [DM] Initialising grid and drawing square by coordinate

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,36 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const squareLength = 40;
+const squareSpace = squareLength + 1;
+const ctx = document.getElementById('grid').getContext('2d');
+
+initialiseGrid();
+drawStartSquare();
+drawEndSquare();
+
+function initialiseGrid() {
+    ctx.fillStyle = "rgb(200,0,0)";
+
+    for (let x = 0, i = 0; i < 7; x += squareSpace, i++) {
+        for (let y = 0, j = 0; j < 6; y += squareSpace, j++) {
+            ctx.fillRect(x, y, squareLength, squareLength);
+        }
+    }
+}
+
+function drawSquare(x, y, c){
+    ctx.fillStyle = "rgb(" + c.r + "," + c.g + "," + c.b + ")";
+
+    let newX = x * squareSpace;
+    let newY = y * squareSpace;
+
+    ctx.fillRect(newX, newY, squareLength, squareLength);
+}
+
+function drawStartSquare() {
+    drawSquare(0, 0, {r: 0, g: 255, b: 0});
+}
+
+function drawEndSquare() {
+    drawSquare(6, 0, {r: 0, g: 0, b: 255});
+}
