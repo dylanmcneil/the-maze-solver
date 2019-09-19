715dbef0a5d0c525a28f98d62bdd1d230d0884e6
Dylan McNeil <dylan_mcneil@hotmail.co.uk>
2019-09-19 22:50:17 +0100

    [DM] Java and Angular web app set up

---
+++ src/main/java/app/User.java
@@ -0,0 +1,59 @@
+package app;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private final String name;
+    private final String email;
+
+    public User(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+        this.name = null;
+        this.email = null;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "app.User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    // standard constructors / setters / getters / toString
+}
--- src/scripts/models/colour.js
+++
@@ -1,7 +0,0 @@
-export class Colour {
-    constructor(r, g, b){
-        this.r = r;
-        this.g = g;
-        this.b = b;
-    }
-}
