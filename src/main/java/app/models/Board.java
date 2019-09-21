533b2e37be12fa97b0d1578164d64f177b080656
Dylan McNeil <dylan_mcneil@hotmail.co.uk>
2019-09-21 22:37:40 +0100

    [DM] Exposing size of square

--- src/main/java/app/models/Board.java
+++ src/main/java/app/models/Board.java
@@ -3,17 +3,19 @@ package app.models;
 import java.util.List;
 
 public class Board {
-    private List<Point> points;
+    private final List<Point> points;
+    private final int squareLength;
 
     public Board(List<Point> points) {
         this.points = points;
+        this.squareLength = (int) Math.sqrt(points.size());
     }
 
     public List<Point> getPoints() {
         return points;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public int getSquareLength() {
+        return squareLength;
     }
 }
